11ac44fc20e9e34eaeda0919293a965fa2defd8b
R. Nazarbek <70977121+NBG9@users.noreply.github.com>
2022-06-22 08:09:35 +0300

    Add files via upload

---
+++ crawl.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+def writeTofile(a):
+    f = open("/Users/Uni/PycharmProjects/WebCrawl/venv/lib/python3.8/imageUrls2.txt", "a")
+    f.write(a)
+    f.write("\n")
+    f.close()
+
+
+# Return all camera links for a specific camera brand
+# Sample camera link: "https://www.flickr.com/cameras/samsung/"
+wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+wd.get() #TODO: Put flickr camera link
+
+search = wd.find_elements(By.TAG_NAME, "a")
+
+search = list(set(search))
+elems = []
+search2 = wd.find_elements(By.TAG_NAME, "a")
+
+for s2 in search2:
+    print(s2.text.strip())
+    try:
+        print(s2.find_element(By.TAG_NAME, "i").text.strip())
+    except:
+        print("".strip())
+
+wd.close()
+wd.quit()
